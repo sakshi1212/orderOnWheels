b533c3cde4c005f5af2ef7ed7fc71486fa5ed53a
Sakshi <skakkar121291@gmail.com>
2019-07-30 01:41:09 +0800

    feat: implement all three api routes

--- config/global.js
+++ config/global.js
@@ -3,9 +3,11 @@ const _ = require('lodash');
 require('./environment');
 
 const { models, sequelize } = require('./sequelize')();
+const { HttpError } = require('../service/error');
 
 module.exports = () => {
   global._ = _;
   global.DB = models;
   global.sequelize = sequelize;
+  global.HttpError = HttpError;
 };
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 require('./config/global')();
 const configApi = require('./api');
 const configSequelize = require('./config/sequelize');
+const errorService = require('./service/error');
 
 const app = express();
 const server = require('http').createServer(app);
@@ -11,7 +12,7 @@ app.use(bodyParser.json());
 
 configSequelize();
 configApi(app);
-
+app.use(errorService.middleware);
 server.listen(8080, () => {
   console.log("Server listening on port 8080!");
 });
