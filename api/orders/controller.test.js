cde94ff055b6d07e6f287bb8a091964a080600bc
Sakshi <skakkar121291@gmail.com>
2019-07-30 17:44:59 +0800

    feat: setup jest and mockhttp

--- api/orders/controller.test.js
+++ api/orders/controller.test.js
@@ -4,6 +4,26 @@ const mockHttp = require('../../test/utils/http/mock');
 
 
 describe('API: Order: controller', () => {
+  describe('create()', () => {
+    test('create a order in DB', async (done) => {
+      const { req, res, next } = mockHttp({
+        request: {
+          body: {
+            "origin": ["22.279800", "114.183940"],
+            "destination": ["22.279680", "114.171690"]
+          }
+        },
+      });
+      const data = await controller.create(req, res, next);
+      console.log('==================', data);
+      expect(Array.isArray(_payload)).toBe(true);
+      done();
+    });
+  });
+});
+
+
+describe('API: Order: controller', () => {
   describe('index()', () => {
     test('returns all orders before any order exists in DB', async (done) => {
       const { req, res, next } = mockHttp({
