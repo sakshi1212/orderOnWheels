4d4c10f4d90799582b336482333491aed5e0e296
Sakshi <skakkar121291@gmail.com>
2019-08-03 22:16:34 +0800

    fix: env variables

--- config/config.js
+++ config/config.js
@@ -1,3 +1,5 @@
+const { mysql } = require('../config/environment');
+
 let config = {
   development: {
     username: "root",
@@ -7,23 +9,22 @@ let config = {
     dialect: "mysql"
   },
   test: {
-    username: "root",
-    password: "123123123",
-    database: "orderOnWheelsTest",
-    host: "127.0.0.1",
+    username: mysql.username,
+    password: mysql.password,
+    database: mysql.name,
+    host: mysql.host,
     dialect: "mysql"
   },
   production: {
-    username: "root",
-    password: "123123123",
-    database: "orderOnWheels",
-    // host: process.env.DB_HOST,
-    host: "192.168.86.207",
-    dialect: "mysql"
+    username: mysql.username,
+    password: mysql.password,
+    database: mysql.name,
+    host: mysql.host,
+    dialect: "mysql",
   }
 };
 
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === 'development') {
   config = config.development;
@@ -36,7 +37,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 
-console.log(`Running sequelize-cli on ${process.env.NODE_ENV} on host: ${config.host}${config.username}${config.password}`);
+console.log(`Running sequelize-cli on ${process.env.NODE_ENV} on host: ${config.host}`);
 
 module.exports = {
   ...config,
--- config/environment/production.js
+++ config/environment/production.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = {
-  // env: 'production',
-  // port: '8080',
-  // mysql: {
-  //   name: "orderOnWheels",
-  //   host: "172.17.0.2",
-  //   username: "root",
-  //   password: "123123123",
-  // },
+  env: 'production',
+  port: '8080',
+  mysql: {
+    name: "orderOnWheels",
+    host: "192.168.86.207",
+    username: "root",
+    password: "123123123",
+  },
 };
